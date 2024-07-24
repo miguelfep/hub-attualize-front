19c8b9b9be9646e56e8112903cc2208e19e858bf
Miguel Fernando Pereira <miguel@attualizetech.com.br>
2024-07-24 16:36:29 -0300

    remove supabase

--- src/app/auth-demo/split/sign-in/layout.jsx
+++
@@ -1,7 +0,0 @@
-import { AuthSplitLayout } from 'src/layouts/auth-split';
-
-// ----------------------------------------------------------------------
-
-export default function Layout({ children }) {
-  return <AuthSplitLayout section={{ title: 'Bem vindo novamente' }}>{children}</AuthSplitLayout>;
-}
